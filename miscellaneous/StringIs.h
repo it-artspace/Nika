9ec6a2a9d701f2419c98cd0ae8c77223283b8d68
jernicozz <cendleader@gmail.com>
2019-12-13 02:46:20 +0300

    today work

---
+++ miscellaneous/StringIs.h
@@ -0,0 +1,13 @@
+//
+//  StringIs.h
+//  task1
+//
+//  Created by Дмитрий Маслюков on 13.12.2019.
+//  Copyright © 2019 Дмитрий Маслюков. All rights reserved.
+//
+
+#ifndef StringIs_h
+#define StringIs_h
+
+
+#endif /* StringIs_h */
